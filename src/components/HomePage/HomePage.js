6baa84d9b40deeb40b2a5da2675674ddb8bbc3ae
Tomer Lilush <tomerxp@gmail.com>
2019-12-07 15:48:48 +0200

    style: removed border from layout

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const HomePage = () => (
-  <div>Welcome, use the navigation bar to explore the app!</div>
+  <div>Welcome, use the navigation bar to explore through the app!</div>
 )
 
 export default HomePage
